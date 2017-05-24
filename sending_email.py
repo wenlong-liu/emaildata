e0790c844e835553009ebc1357e6035887076d94
Wenlong Liu <wenlongliu@yahoo.com>
2017-05-24 16:29:09 -0400

    Update the email sending using yagmail

--- sending_email.py
+++ sending_email.py
@@ -1,84 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 10 16:32:22 2017
-This scripts are used to send out data from Pasture to Wenlong from field.
-
-Key features:
-- Parse and send out all .par files
-- Send out email at certain intervals: such as one day.
-@author: wliu14
+Created on Wed May 24 11:42:02 2017
+_version_ : 0.2
+Update:
+ - Use yagmail to wrap the email sending scripts.
+ - Not saving password in the scripts.
+@author: Wenlong
 """
-
-from email import encoders
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-
-import smtplib
-import time
+import yagmail
 import glob
+import time
 import os
-import logging
 
-def _get_files(path):
-    # parse and find out all the files endwith .par.
+def _get_filename(path, _type):
+    '''
+    Obtain the specify files in the target folder.
+    Parameter:
+    -path, such as 'F:\\'
+    -_type, file type, such as: .par
+    '''
     files = list()
 
-    for filename in glob.glob(os.path.join(path, '*.par')):
-        files.append(filename)
-
+    for filename in glob.glob(os.path.join(path, '*' + _type)):
+        files.append(filename)        
     return files
-
-def send_email(path):
-
-    #information of the email address.
-    from_addr = 'wenlongliu853@gmail.com'
-    email_password = 'XXXX'
-    to_addr = 'wenlongliu853@gmail.com'
-
-    #send attachment via email.
-    msg = MIMEMultipart()
-    msg['From'] = from_addr
-    msg['To'] = to_addr
-    msg['Subject'] = 'This is a testing email.'
-
-    #Attachement information.
-    files = _get_files(path)
-    msg.attach(MIMEText('This is a testing email to send out the Plymouth data', 'plain', 'utf-8'))
-
-    for filename in files:
-    #Adding attachments.
-        with open(filename, 'rb') as f:
-        # Set the name and format of the attachment:
-            mime = MIMEBase('text', 'plain', filename=filename)
-        # Header information:
-            mime.add_header('Content-Disposition', 'attachment', filename=filename)
-            mime.add_header('Content-ID', '<0>')
-            mime.add_header('X-Attachment-Id', '0')
-        # Read in attachment:
-            mime.set_payload(f.read())
-        # Decode the information:
-            encoders.encode_base64(mime)
-        # Add files into attachment.
-            msg.attach(mime)
-
-    smtp_server = 'smtp.gmail.com'
-    smtp_port = 587
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.set_debuglevel(1)
-    server.login(from_addr, email_password)
-    server.sendmail(from_addr, to_addr, msg.as_string())
-    server.quit()
-
+   
+def send_email(username, to, subject, body, attachments):
+    '''
+    Wrapper of yagmail to send out emails.
+    '''
+    # for the first time user, a popup will be shown to ask for password.
+    # Note the password only need once if you save it in your computer.
+    yag = yagmail.SMTP(username)                 
+    yag.send(to=to, subject=subject, contents=[body] + attachments)
+    
 if __name__ == '__main__':
-    path = 'C:\\s-canV5.0\\Results\\ORIGINAL'
-    interval = 600 #Unit: second
-    while True:
+    '''
+    Get inputs from the manual input.
+    '''
+    path = input('Please enter the file path:')
+    fileType = input('Please enter the file extention, such as .par:')
+    username = input('Please enter the user email address:')
+    to = input('Please enter the target address:')
+    subject = input('Please enter the subject of email:')
+    body = input('Please enter the contents of the email.')
+    interval = int(input('Please set the sending interval, unit: second:'))
+    
+    # To break the loop and exit the program, use CTRL + C.        
+    while True:       
         try:
-            send_email(path)
-            print('\n Sending one email!\n')
+            attachments = _get_filename(path, fileType)
+            send_email(username,username,subject=subject, body=body, 
+                   attachments=attachments)
+            print('Send one email.')
+            
         except:
-            print('\n error, try again later.\n ')
-        finally:
-            time.sleep(interval)
+            print('Bad connection, try it again.')       
+     
+        finally:       
+            time.sleep(interval)            
