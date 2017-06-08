757740f2a551ebb28bace34ca95ad1532cc7c4a9
Wenlong Liu <wenlongliu@yahoo.com>
2017-06-08 14:02:14 -0400

    Update configuration file.
    V0.3 Update:
     - Using Json files as configuration to save time;
     - Adding attachments from different folders and file types.

--- sending_email.py
+++ sending_email.py
@@ -1,61 +1,80 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed May 24 11:42:02 2017
-_version_ : 0.2
-Update:
+
+V0.2 Update :
  - Use yagmail to wrap the email sending scripts.
  - Not saving password in the scripts.
-@author: Wenlong
+
+@author: Wenlong Liu 
+wliu14@ncsu.edu
 """
 import yagmail
 import glob
 import time
 import os
+import json
 
-def _get_filename(path, _type):
-    '''
+
+def _configuration(filename):
+    """
+    Load configuration parameters from existing json file.
+    :param filename: the json file to store parameters.
+    :return: A dictionary with all the parameters.
+    """
+    with open(filename) as configuration:
+        data = json.load(configuration)
+    return data
+
+
+def _get_filename(Path, FileType):
+    """
     Obtain the specify files in the target folder.
-    Parameter:
-    -path, such as 'F:\\'
-    -_type, file type, such as: .par
-    '''
+    :param:
+    -Path: such as 'F:\\'
+    -FileType: file type, such as: .par
+    :return:
+    A list that contains all the names of targeted files in specific paths.
+    """
     files = list()
-
-    for filename in glob.glob(os.path.join(path, '*' + _type)):
-        files.append(filename)        
+    for filename in glob.glob(os.path.join(Path, '*' + FileType)):
+        files.append(filename)
     return files
-   
+
+
 def send_email(username, to, subject, body, attachments):
-    '''
+    """
     Wrapper of yagmail to send out emails.
-    '''
+    """
     # for the first time user, a popup will be shown to ask for password.
-    # Note the password only need once if you save it in your computer.
-    yag = yagmail.SMTP(username)                 
+    yag = yagmail.SMTP(username)
+
     yag.send(to=to, subject=subject, contents=[body] + attachments)
-    
+
+
 if __name__ == '__main__':
-    '''
-    Get inputs from the manual input.
-    '''
-    path = input('Please enter the file path:')
-    fileType = input('Please enter the file extention, such as .par:')
-    username = input('Please enter the user email address:')
-    to = input('Please enter the target address:')
-    subject = input('Please enter the subject of email:')
-    body = input('Please enter the contents of the email.')
-    interval = int(input('Please set the sending interval, unit: second:'))
-    
-    # To break the loop and exit the program, use CTRL + C.        
-    while True:       
+    # load configurations from json.
+    config = _configuration('configuration.json')
+    username = config['SendAddress']
+    to = config['ReceiveAddress']
+    subject = config['Subject']
+    body = config['Content']
+    interval = config['Interval']
+    attachment = config['Attachments']
+
+    while True:
+        attachments = list()
         try:
-            attachments = _get_filename(path, fileType)
-            send_email(username,username,subject=subject, body=body, 
-                   attachments=attachments)
+            for item in attachment:
+                attachments += _get_filename(attachment[item]['Path'],
+                                             attachment[item]['FileType'])
+
+            send_email(username, username, subject=subject, body=body,
+                       attachments=attachments)
+
             print('Send one email.')
-            
         except:
-            print('Bad connection, try it again.')       
-     
-        finally:       
-            time.sleep(interval)            
+            print('Bad connection, try it again.')
+
+        finally:
+            time.sleep(interval)
