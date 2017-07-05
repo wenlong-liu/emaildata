c693fd668e6d204829cdfcfa8049036af0511038
wliu14 <wenlongliu@yahoo.com>
2017-07-04 22:49:34 -0400

    Update lisence, setup tools.

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'emaildata',
+    packages = ['emaildata'], # this must be the same as the name above
+    version = '0.1',
+    description = 'A Python package to automatically push notifications and send data.',
+    author = 'Wenlong Liu',
+    author_email = 'wliu14@ncsu.edu',
+    url = 'https://github.com/wliu2016/emaildata', # use the URL to the github repo
+    download_url = 'https://github.com/wliu2016/emaildata/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords = ['Python', 'email', 'notification', 'extract data'], # arbitrary keywords
+    classifiers = [],
+)
